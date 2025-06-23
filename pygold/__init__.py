d9d7f9de4105a29e45370118074bc774c84aa7bb
Byron Selvage <byron.selvage@nrel.gov>
2025-06-23 11:31:07 -0600

    Import visibility

--- pygold/__init__.py
+++ pygold/__init__.py
@@ -3,11 +3,13 @@ pyGOLD: Python Global Optimization Library for Development
 """
 
 # Direct access to common functions
+from . import problems
 from .default_runners import run_solvers
 from .postprocessing import compute_performance_ratios, compute_performance_profiles, plot_performance_profiles
 from .cocopp_interface import configure_testbed
 
 __all__ = [
+    'problems',
     'run_solvers',
     'compute_performance_ratios',
     'compute_performance_profiles',
--- pygold/problems/__init__.py
+++ pygold/problems/__init__.py
@@ -5,3 +5,10 @@ This module contains standard benchmark problems and real-world application
 inspired benchmark problems.
 """
 
+from . import floris_problems
+from . import standard_problems
+
+__all__ = [
+    "floris_problems",
+    "standard_problems"
+]
