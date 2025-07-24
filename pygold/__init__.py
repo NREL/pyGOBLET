694fa47115b61f29af7d2c56ee1bce46a0769687
Byron Selvage <byron.selvage@nrel.gov>
2025-07-24 08:56:08 -0600

    Update public functions

--- pygold/__init__.py
+++ pygold/__init__.py
@@ -4,12 +4,16 @@ pyGOLD: Python Global Optimization Library for Development
 
 # Direct access to common functions
 from . import problems
+from .problems import floris_problems
+from .problems import standard_problems
 from .default_runners import run_solvers
 from .postprocessing import postprocess_data
 from .cocopp_interface import configure_testbed
 
 __all__ = [
     'problems',
+    'floris_problems',
+    'standard_problems',
     'run_solvers',
     'postprocess_data',
     'configure_testbed'
--- pygold/cocopp_interface/__init__.py
+++ pygold/cocopp_interface/__init__.py
@@ -4,9 +4,8 @@ COCOPP Interface Module
 This module provides integration with COCOPP (COmparing Continuous Optimizers)
 for generating performance plots and tables.
 """
-from .interface import log_coco_from_results, configure_testbed
+from .interface import configure_testbed
 
 __all__ = [
-    'log_coco_from_results',
     'configure_testbed'
 ]
