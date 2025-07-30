732211eb3c96e5e38f6246b38ba3ed2ab4495236
Byron Selvage <byron.selvage@nrel.gov>
2025-07-30 11:09:12 -0600

    Function getter public visibility

--- pygold/__init__.py
+++ pygold/__init__.py
@@ -1,5 +1,5 @@
 """
-pyGOLD: Python Global Optimization Library for Development
+pyGOLD: Python Global Optimization Library (for Development)
 """
 
 # Direct access to common functions
@@ -9,11 +9,13 @@ from .problems import standard_problems
 from .default_runners import run_solvers
 from .postprocessing import postprocess_data
 from .cocopp_interface import configure_testbed
+from .problems.standard_problems import get_standard_problems
 
 __all__ = [
     'problems',
     'floris_problems',
     'standard_problems',
+    'get_standard_problems',
     'run_solvers',
     'postprocess_data',
     'configure_testbed'
